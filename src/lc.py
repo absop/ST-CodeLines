4e14b3c1bee63db9347f2ed78fc31b2591c292c5
叶芝秋 <zlang0@163.com>
2021-10-21 17:02:00 +0800

    Added support for OSX

--- src/lc.py
+++ src/lc.py
@@ -7,26 +7,43 @@ elif os.name == "posix":
     from _ctypes import dlclose as _dlclose
 
 
-def count(path):
-    return c_count(bytes(path, encoding=path_encoding))
+def make_counter(function=None, encoding=None):
+    global count
+    encoding = encoding or getattr(make_counter, 'encoding', None)
+    function = function or getattr(make_counter, 'function', None)
+    make_counter.encoding = encoding
+    make_counter.function = function
+
+    if function is None or encoding is None:
+        return
+
+    count = lambda path: function(bytes(path, encoding=encoding))
 
 
 def set_encoding(encoding):
-    global path_encoding
-    path_encoding = encoding
+    make_counter(encoding=encoding)
 
 
 def load_shared_object(so):
-    global module, c_count
-    module = ctypes.cdll.LoadLibrary(so)
+    global module
+    try:
+        module = ctypes.cdll.LoadLibrary(so)
 
-    c_count = module.lines_count
-    c_count.argtypes = (ctypes.POINTER(ctypes.c_char),)
-    c_count.restype = ctypes.c_int
+        c_count = module.lines_count
+        c_count.argtypes = (ctypes.POINTER(ctypes.c_char),)
+        c_count.restype = ctypes.c_int
+        make_counter(function=c_count)
+    except:
+        module = None
+        def c_count(path):
+            with open(path, 'rb') as file:
+                return file.read().count(b'\n')
+        make_counter(function=c_count)
 
 
 def unload_shared_object():
-    try:
-        _dlclose(module._handle)
-    except Exception as e:
-        raise IOError("Could not unload shared object library.")
+    if module:
+        try:
+            _dlclose(module._handle)
+        except Exception as e:
+            raise IOError("Could not unload shared object library.")
